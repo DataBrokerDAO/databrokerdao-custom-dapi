21ed333486731fed4dfe05a42507dbf3d6a66d0b
PJ <peter-jan@settlemint.com>
2018-05-07 14:33:53 +0200

    fix: add logging

--- src/server.js
+++ src/server.js
@@ -47,8 +47,7 @@ app.get('/unsubscribe', (req, res, next) => {
   registry
     .unsubscribe(email, sensorid)
     .then(response => {
-      let unsubscribedUrl =
-        rtrim(process.env.DAPP_BASE_URL, '/') + '/unsubscribed';
+      let unsubscribedUrl = rtrim(process.env.DAPP_BASE_URL, '/') + '/unsubscribed';
       res.redirect(unsubscribedUrl);
     })
     .catch(error => {
@@ -62,10 +61,7 @@ app.post('/:sensorid/data', async (req, res, next) => {
   const sensorCsvUrl = req.body.url;
   const sensorCsvData = req.body.data;
 
-  if (
-    typeof sensorCsvUrl === 'undefined' &&
-    typeof sensorCsvData === 'undefined'
-  ) {
+  if (typeof sensorCsvUrl === 'undefined' && typeof sensorCsvData === 'undefined') {
     return res.sendStatus(400);
   }
 
@@ -128,7 +124,7 @@ function bootstrap() {
 async function handlePurchase(purchase) {
   const sensor = await store.getSensorForKey(purchase.sensor);
   if (!sensor) {
-    console.log(`Error: could not find sensor for stream ${purchase.sensor}`);
+    console.log(`Error: could not find sensor ${purchase.sensor}`);
     return;
   }
 
@@ -138,10 +134,7 @@ async function handlePurchase(purchase) {
     email = purchase.email;
   } else {
     email = ecies
-      .decryptMessage(
-        Buffer.from(process.env.SERVER_PRIVATE_KEY, 'hex'),
-        Buffer.from(purchase.email)
-      )
+      .decryptMessage(Buffer.from(process.env.SERVER_PRIVATE_KEY, 'hex'), Buffer.from(purchase.email))
       .toString('ascii');
   }
 
