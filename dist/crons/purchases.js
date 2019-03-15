8b5b92829b74fc2bf26510dd018b5414424553e7
Main <nvde1960@gmail.com>
2019-03-15 10:49:28 +0100

    Remove dist folder

--- dist/crons/purchases.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const purchase_1 = require("../dapi/purchase");
-async function sensorPurchaseCron() {
-    purchase_1.updateSensorPurchases();
-}
-exports.sensorPurchaseCron = sensorPurchaseCron;
-//# sourceMappingURL=purchases.js.map
